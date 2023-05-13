15b3329b5cd82e1d0e0e0ad1ce2b4ed3e36e44a9
noone <kolosochek@gmail.com>
2023-05-13 10:06:07 +0200

    client branch heroku fix

--- client/src/http/index.ts
+++ client/src/http/index.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const $host = axios.create({
-    baseURL: `${process.env.REACT_APP_SERVER_URL}:${process.env.REACT_APP_SERVER_PORT}`
+    baseURL: `${process.env.SERVER_URL}:${process.env.SERVER_PORT}`
 })
 
 const $authHost = axios.create({
-    baseURL: `${process.env.REACT_APP_SERVER_URL}:${process.env.REACT_APP_SERVER_PORT}`
+    baseURL: `${process.env.SERVER_URL}:${process.env.SERVER_PORT}`
 })
 
 
