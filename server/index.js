7d05a6a9a109591fc095fc66bb04df8f0d99240b
noone <kolosochek@gmail.com>
2023-05-14 14:52:39 +0200

    heroku fix

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const errorHandler = require('./middleware/ErrorHanlingMiddleware')
 const path = require('path')
 
 const app = express()
-app.use(cors())
+//app.use(cors())
 app.use(express.json())
 app.use(express.static(path.resolve(__dirname, 'static')))
 app.use(fileUpload({}))
